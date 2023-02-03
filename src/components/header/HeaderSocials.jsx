9652361e6c4b9ab2f4ad7f1d764ef7fdec74e2da
willmccarten <william.mccarten@ufl.edu>
2023-02-03 18:52:09 -0500

    Add pics to assets and header

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://linkedin.com/in/william-mccarten' target="_blank"></a>
+        <a href='https://github.com/willmccarten' target="_blank"></a>
+        <a href='https://twitter.com/willmccarten' target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
