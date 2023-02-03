dc86af3da95e44ac21c086d8bc150f5876364fe1
willmccarten <william.mccarten@ufl.edu>
2023-02-03 04:34:01 -0500

    Setting up directories for components Creating files for components Importing components to main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Nav from './components/nav/Nav'
+import Testimonials from './components/testimonials/Testimonials'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import About from './components/about/About'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+    </>
   )
 }
 
