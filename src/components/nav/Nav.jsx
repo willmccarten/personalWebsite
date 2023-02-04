07bbfa49504690c5c2c9dbe8874b775568c5211e
willmccarten <william.mccarten@ufl.edu>
2023-02-04 02:20:17 -0500

    adding icons to nav bar and handling section tags

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {IoIosHome} from 'react-icons/io'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsBook} from 'react-icons/bs'
+import {RiServiceLine} from 'react-icons/ri'
+import {BiMessageDetail} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className="nav">
+      <a href="#header"><IoIosHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BsBook/></a>
+      <a href="#services"><RiServiceLine/></a>
+      <a href="#contacts"><BiMessageDetail/></a>
+    </nav>
   )
 }
 
