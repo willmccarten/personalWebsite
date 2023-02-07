f0322ac84a47242137ef6804190d1151b2666859
willmccarten <william.mccarten@ufl.edu>
2023-02-06 23:52:02 -0500

    html layout of Experience component

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>The Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience-container'>
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+            
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+
+        </div>
+      </div>
+
+    </section>
   )
 }
 
