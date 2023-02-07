d97ce39229ad047204b9101e903185e9f30a5343
willmccarten <william.mccarten@ufl.edu>
2023-02-07 00:34:37 -0500

    Fix container bug in experience.jsx

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
       <h5>The Skills I Have</h5>
       <h2>My Experience</h2>
 
-      <div className='container experience-container'>
+      <div className='container experience__container'>
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
