4ca1bc4cb588dc2313f7518a65c971e2dd084be0
willmccarten <william.mccarten@ufl.edu>
2023-02-08 19:22:39 -0500

    Portfolio comp html layout, header fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,14 +10,14 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>William McCarten</h1>
-        <h5 className='text-light'>software developer</h5>
+        <h5 className='text-light'>Software Developer</h5>
         <CTA />
         <HeaderSocials />
 
         <div className="me">
           <img src={ME} alt="me"/>
         </div>
-
+        
         <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
       
