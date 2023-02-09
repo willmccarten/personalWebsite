7f32f1711b6bb85154177b47be782b21feb13b59
willmccarten <william.mccarten@ufl.edu>
2023-02-08 19:47:38 -0500

    portfolio card arr make js funct for html comp

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,50 @@ import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Portfolio Item 1',
+    github: 'https://github.com/willmccarten',
+    demo: 'https://github.com/willmccarten'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Portfolio Item 2',
+    github: 'https://github.com/willmccarten',
+    demo: 'https://github.com/willmccarten'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Portfolio Item 3',
+    github: 'https://github.com/willmccarten',
+    demo: 'https://github.com/willmccarten'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Portfolio Item 4',
+    github: 'https://github.com/willmccarten',
+    demo: 'https://github.com/willmccarten'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Portfolio Item 5',
+    github: 'https://github.com/willmccarten',
+    demo: 'https://github.com/willmccarten'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Portfolio Item 6',
+    github: 'https://github.com/willmccarten',
+    demo: 'https://github.com/willmccarten'
+  }
+]
 
 const Portfolio = () => {
   return (
@@ -15,78 +59,25 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="portfolio item"/>
 
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/willmccarten" className='btn' target='_blank'>GitHub</a>
-            <a href="https://github.com/willmccarten" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title}/>
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="portfolio item"/>
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn' target='_blank'>GitHub</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+              </article>
+            )
+          })
+        }
 
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/willmccarten" className='btn' target='_blank'>GitHub</a>
-            <a href="https://github.com/willmccarten" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="portfolio item"/>
-
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/willmccarten" className='btn' target='_blank'>GitHub</a>
-            <a href="https://github.com/willmccarten" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="portfolio item"/>
-
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/willmccarten" className='btn' target='_blank'>GitHub</a>
-            <a href="https://github.com/willmccarten" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="portfolio item"/>
-
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/willmccarten" className='btn' target='_blank'>GitHub</a>
-            <a href="https://github.com/willmccarten" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="portfolio item"/>
-
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com/willmccarten" className='btn' target='_blank'>GitHub</a>
-            <a href="https://github.com/willmccarten" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        
       </div>
     </section>
   )
