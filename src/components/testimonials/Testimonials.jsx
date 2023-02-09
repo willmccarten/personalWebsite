42588a920617a5372ad09003bbe388d05ea1f6f8
willmccarten <william.mccarten@ufl.edu>
2023-02-08 21:35:30 -0500

    dynamic gen testimonial comp w/arr

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,29 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Ernest Client',
+    review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, accusamus. Dolorum doloremque alias vero? Alias officia, ut neque delectus laborum rerum nihil dolores necessitatibus. Nemo laborum cum quia ipsum est.'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Ernest Client',
+    review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, accusamus. Dolorum doloremque alias vero? Alias officia, ut neque delectus laborum rerum nihil dolores necessitatibus. Nemo laborum cum quia ipsum est.'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Ernest Client',
+    review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, accusamus. Dolorum doloremque alias vero? Alias officia, ut neque delectus laborum rerum nihil dolores necessitatibus. Nemo laborum cum quia ipsum est.'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Ernest Client',
+    review: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, accusamus. Dolorum doloremque alias vero? Alias officia, ut neque delectus laborum rerum nihil dolores necessitatibus. Nemo laborum cum quia ipsum est.'
+  }
+]
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
@@ -12,19 +35,19 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-
-        <article className='testimonial'>
-          <div className="client__avatar">
-            <img src={AVTR1} alt="client avatar" />
-          </div>
-          <h5 className='client__name'>Ernest Client</h5>
-            <small className='client__review'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Unde, accusamus. Dolorum doloremque alias vero?
-              Alias officia, ut neque delectus laborum rerum nihil 
-              dolores necessitatibus. Nemo laborum cum quia ipsum est.
-            </small>
-        </article>
-
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+              <article key={index} className='testimonial'>
+                <div className="client__avatar">
+                  <img src={avatar} alt="client avatar" />
+                </div>
+                <h5 className='client__name'>{name}</h5>
+                  <small className='client__review'>{review}</small>
+              </article>
+            )
+          })
+      }
       </div>
     </section>
   )
