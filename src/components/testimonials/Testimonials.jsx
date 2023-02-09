29180b863657b8fe86dc4e3baee151adbef7a1f1
willmccarten <william.mccarten@ufl.edu>
2023-02-08 19:58:01 -0500

    basic html settup for testimonial comp

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+
+        <article className='testimonial'>
+          <div className="client__avatar">
+            <img src={AVTR1} alt="client avatar" />
+            <h5 className='client__name'>Ernest Client</h5>
+            <small className='client__review'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Unde, accusamus. Dolorum doloremque alias vero?
+              Alias officia, ut neque delectus laborum rerum nihil 
+              dolores necessitatibus. Nemo laborum cum quia ipsum est.
+            </small>
+          </div>
+        </article>
+        
+      </div>
+    </section>
   )
 }
 
