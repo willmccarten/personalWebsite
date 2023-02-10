8f5da0d9eecfdf7cdb63b8ff20b1b1366b4f06fc
willmccarten <william.mccarten@ufl.edu>
2023-02-10 13:28:52 -0500

    html for contact comp (full layout)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
 
 const Contact = () => {
   return (
-    <section id='contact'>Keep the streak alive</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+
+          <article className='contact__option'>
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>williampmccarten@gmail.com</h5>
+            <a href="mailto:williampmccarten@gmai.com" target="_blank">Send a Message</a>
+          </article>
+        </div>
+
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
