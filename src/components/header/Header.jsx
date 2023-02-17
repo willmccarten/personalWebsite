5cba28e09b6c97ee1b6c2aba53bf938f632b9733
willmccarten <william.mccarten@ufl.edu>
2023-02-17 04:46:31 -0500

    header styling update, bug fix in portfolio comp

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me_fr_fr.png'
+import ME from '../../assets/me_3.JPG'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
