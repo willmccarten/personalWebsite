b44e9a6d6fa5a344b56d8838eb9439943f3ba4f2
willmccarten <william.mccarten@ufl.edu>
2023-02-16 23:46:02 -0500

    Use Emailjs to give contact comp functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hh24pav', 'template_4xyxroj', form.current, 'oq25AdYbf2sBKIKjd')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+  };
+  
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -15,11 +33,11 @@ const Contact = () => {
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>williampmccarten@gmail.com</h5>
-            <a href="mailto:williampmccarten@gmai.com" target="_blank">Send a Message</a>
+            <a href="mailto:williampmccarten@gmail.com" target="_blank">Send a Message</a>
           </article>
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
