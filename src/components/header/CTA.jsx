f6779161d52d7bdf68dd47a8ff02d680af7f2b25
willmccarten <william.mccarten@ufl.edu>
2023-09-18 12:21:51 -0400

    adding in new resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/William McCarten - CV.pdf'
+import CV from '../../assets/William McCarten - Professional Resume (3).pdf'
 
 const CTA = () => {
     return (
