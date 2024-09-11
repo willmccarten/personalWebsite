ed3de6c074033df76b5cbbce5034011cf15e5e43
Will <73602111+willmccarten@users.noreply.github.com>
2024-09-11 18:24:14 +0100

    change email button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
           <article className='contact__option'>
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5>williampmccarten@gmail.com</h5>
+            <h5>From your email app</h5>
             <a href="mailto:williampmccarten@gmail.com" target="_blank">Send a Message</a>
           </article>
         </div>
